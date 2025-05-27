1a43f2cf0c657f98f6ac9fc4b0c76d226713b951
AlphaBs <ksi123456ab@naver.com>
2025-05-27 17:27:26 +0900

    feat: FishSyncClient 에서 서버 관련된 부분 제거
    다른 패키지로 이동

--- src/Server/BucketSyncActions/IBucketSyncActionCollectionHandler.cs
+++
@@ -1,8 +0,0 @@
-using FishSyncClient.Files;
-
-namespace FishSyncClient.Server.BucketSyncActions;
-
-public interface IBucketSyncActionCollectionHandler
-{
-    Task Handle(ISyncFileCollection files, IEnumerable<BucketSyncAction> actions, CancellationToken cancellationToken);
-}
--- src/Server/BucketSyncFile.cs
+++
@@ -1,8 +0,0 @@
-namespace FishSyncClient.Server;
-
-public class BucketSyncFile
-{
-    public string? Path { get; set; }
-    public long Size { get; set; }
-    public string? Checksum { get; set; }
-}
